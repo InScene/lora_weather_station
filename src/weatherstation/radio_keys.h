dbaed2738aa88c0a2077346f339ec76c8cbf58cc
Christian Mertens <mertens.chr@gmail.com>
2020-09-15 08:42:06 +0200

    Changed name of ino file and moved all src files into sub folder to open it in the arduino ide

--- src/weatherstation/radio_keys.h
+++ src/weatherstation/radio_keys.h
@@ -3,13 +3,13 @@
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-static const u1_t PROGMEM APPEUI[8]={  };
+static const u1_t PROGMEM APPEUI[8]={ };
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]={  };
+static const u1_t PROGMEM DEVEUI[8]={ };
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
 // The key shown here is the semtech default key.
-static const u1_t PROGMEM APPKEY[16] = {  };
+static const u1_t PROGMEM APPKEY[16] = { };
